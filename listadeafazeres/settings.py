62e3aab9b7a059a6ca00b8f091e1abd3a7a86ce8
Pedro <necromancerdoidao@outlook.com>
2023-03-10 08:28:06 -0300

    customizada class User, customizada class UserManager
    close #18, close #19

--- listadeafazeres/settings.py
+++ listadeafazeres/settings.py
@@ -127,4 +127,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+AUTH_USER_MODEL = 'base.User'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
